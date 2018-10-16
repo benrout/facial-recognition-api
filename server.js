0f93b81124a8335231e79003b6d1da88f47694f8
Ben Rout <benadamrout@gmail.com>
2018-10-16 16:07:53 +0100

    Moves routes into separate files

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
 
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
 const db = knex({
     client: 'pg',
     connection: {
@@ -19,86 +24,21 @@ app.use(bodyParser.json())
 app.use(cors());
 
 // Root Route
-app.get('/', (req, res) => {
-    res.send(database.users);
-})
+app.get('/', (req, res) => { res.status(200).json('All good') })
 
 // Sign In
-app.post('/signin', (req, res) => {
-    db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if (isValid) {
-                return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json(err))
-            } else {
-                res.status(400).json('Incorrect credentials');
-            }
-        })
-        .catch(err => res.status(400).json(err))
-})
+app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt) })
 
 // Register
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                    .insert({
-                        email: loginEmail[0],
-                        name: name,
-                        joined: new Date()
-                    })
-                    .returning('*')
-                    .then(user => {
-                        res.json(user[0]);
-                    })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-    })
-        .catch(err => res.status(400).json(err))
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
 // User Profile
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({ id: id })
-        .then(user => {
-            if (user.length) {
-                res.json(user[0])
-            } else {
-                res.status(400).json('User not found')
-            }
-        })
-        .catch(err => res.status(400).json('Error getting user'))
-})
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
 
 // Update User Image Count
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            res.json(entries[0]);
-        })
-        .catch(err => res.status(400).json('Unable to retrieve user score'))
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 // App Listen
 app.listen(3000, () => {
-    console.log("App is running on port 3000");
+    console.log("App is listening on port 3000");
 })
